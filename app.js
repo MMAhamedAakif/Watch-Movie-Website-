4b530fc518e4972dc62c7f16b4b4544955f45875
Ahamed Aakif <mmahamedaakif@gmail.com>
2025-12-29 20:16:04 +0530

    update : js file fixed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-//step 1: get DOM
+//step 1: get DOM           
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item'); 
 let timeDom = document.querySelector('.carousel .time');
 
 
